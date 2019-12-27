72159634c8f0bf59fe35e1460ad47bb05c6fe315
Michal Šiman <m.siman@sbs-nepron.com>
2019-12-28 00:08:36 +0100

    ms

--- hra.py
+++ hra.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/python3
 
 # import knihoven
@@ -6,20 +7,20 @@ import os
 import time
 
 
-# definice jmena hrace
+# definice jmena hrace, pozdeji se bude ptat
 jmeno = "David"
 jmeno2 = "Davide"
 
 
-# nacteni mapy
+# nacteni mapy (mapa a pozice by se mela nacist automaticky z posledni hry)
 mapa = "prvni.mapa"
-f=open(mapa, "r")
+f=open(mapa, "r", encoding="utf-8")
 if f.mode == 'r':
     f1=f.readlines()
     for a in f1:
         print(a)
 f.close()
-time.sleep(5)
+time.sleep(2)
 
 
 # definice os
@@ -34,9 +35,13 @@ while True:
     f.write("y = %d\r\n" % y)
     f.write("x = %d\r\n" % x)
     f.close()
+    print("\033[44mTextová hra VLÁDCE HULIXONU\033[0m \033[43m(c) 2019 DaMiSi\033[0m")
+    print()
     print(jmeno2, ", nacházíš se na", x, y)
-    print("Mužeš jít na \033[4mS\033[0mever, \033[4mV\033[0mýchod, \033[4mZ\033[0mápad, \033[4mJ\033[0mih a můžeš se\033[4mB\033[0mrat předmět nebo u\033[4mK\033[0mončit hru.")
-    print("Co teď udelas?")
+    print()
+    print("\033[93mMužeš jít na \033[4mS\033[0m\033[93ever, \033[4mV\033[0m\033[93ýchod, \033[4mZ\033[0m\033[93ápad, \033[4mJ\033[0m\033[93ih a můžeš se\033[4mB\033[0m\033[93rat předmět nebo u\033[4mK\033[0m\033[93ončit hru.\033[0m")
+    print()
+    print("Co teď uděláš? \033[90m(stiskni příslušnou klávesu)\033[0m")
 
     input_char = msvcrt.getch().decode("utf-8")
 

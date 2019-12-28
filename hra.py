07a03165d87bcaf90599c6b752f8b7bb6aed8a4d
Siman David <59288790+simandavid@users.noreply.github.com>
2019-12-28 15:10:20 +0100

    ds

--- hra.py
+++ hra.py
@@ -9,9 +9,9 @@ from array import *
 
 mapa = []
 
-for i in range(5):
+for i in range(20):
     temp = []
-    for j in range(5):
+    for j in range(20):
         temp.append(0)
     mapa.append(temp)
 
@@ -27,10 +27,12 @@ f=open(nazevmapy, "r", encoding="utf-8")
 if f.mode == 'r':
     f1=f.readlines()
     for a in f1:
-        print(a)
-        mapa[1][1]="aaaaa"
+        if a[0] != "#":
+            abc = (a.split(";"))
+            print(abc)
+            mapa[int(abc[0])][int(abc[1])]=abc[2]
 f.close()
-time.sleep(2)
+time.sleep(5)
 
 
 # definice os
@@ -47,8 +49,7 @@ while True:
     f.close()
     print("\033[44mTextová hra VLÁDCE HULIXONU\033[0m \033[43m(c) 2019 DaMiSi\033[0m")
     print()
-    print(jmeno2, ", nacházíš se na", x, y)
-    print()
+    print(jmeno2,",")
     print(mapa[x][y])
     print()
     print("\033[93mMužeš jít na \033[4mS\033[0m\033[93ever, \033[4mV\033[0m\033[93ýchod, \033[4mZ\033[0m\033[93ápad, \033[4mJ\033[0m\033[93ih a můžeš se\033[4mB\033[0m\033[93rat předmět nebo u\033[4mK\033[0m\033[93ončit hru.\033[0m")
